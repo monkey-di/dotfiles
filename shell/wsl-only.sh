0b8acdea86db0bb5f5fc26fa398c16d5dedbedd2
D <ravencler@gmail.com>
2026-07-13 08:49:03 +0700

    Add wsl config

--- shell/wsl-only.sh
+++ shell/wsl-only.sh
@@ -1,5 +1,15 @@
 if grep -qi microsoft /proc/version 2>/dev/null; then
   # Тут код, который нужен только в WSL
-  export BROWSER="/mnt/c/Windows/explorer.exe"
-  alias open='explorer.exe'
+  #export BROWSER="/mnt/c/Windows/explorer.exe"
+  #alias open='explorer.exe'
+  # v2rayN mixed proxy from Windows host
+  WIN_HOST=$(ip route show | grep -i default | awk '{print $3}')
+  export HTTP_PROXY="http://$WIN_HOST:10808"
+  export HTTPS_PROXY="http://$WIN_HOST:10808"
+  export NO_PROXY="localhost,127.0.0.1,::1"
+  export PATH="$HOME/.local/bin:$PATH"
+  
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
